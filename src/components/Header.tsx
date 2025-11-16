0432471479ea8f20419977fd871ff926d4e54a35
Jyoti prakash Rout <jyotiprakashrout574@gmail.com>
2025-11-16 17:19:01 +0530

    Refactor App and add Hero and Movies components
    Replaces default Vite template in App.tsx with new layout using Header, Hero, and Movies components. Implements a styled Header and adds placeholder Hero and Movies components for future development.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 const Header = () => {
-  return <div></div>;
+  return (
+    <>
+      <header className="relative top-0 left-0 right-0 z-100 py-5">
+        <div className="max-w-6xl mx-auto px-6 md:px-6 flex justify-between items-center">
+          <h1 className="text-xl lg:text-4xl font-bold text-red-600">JFLIX</h1>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
